5f485b3bd5314a61dd6303bff783904247222229
Facundo <facumoya2015@gmail.com>
2022-07-28 16:29:34 -0300

    estilos casi terminados

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -9,7 +9,7 @@ export default function NavBar({onHome}) {
     return (
         <nav className="nav">
             <div className="div-img">
-                <img className="imagen-inicio" src="https://upload.wikimedia.org/wikipedia/commons/b/b1/Rick_and_Morty.svg" alt="" onClick={goToHome}/>
+                <img className="imagen-inicio" src={require('./Rick_and_Morty.png')} onClick={goToHome} alt=""/>
             </div>
         </nav>
     )
